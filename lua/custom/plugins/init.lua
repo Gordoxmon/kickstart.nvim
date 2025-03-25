ab88ce8ae2434acfee66f97ece0dacef8941a49e
Luís Santos <filica812@gmail.com>
2025-03-25 11:02:58 +0000

    activity watch watcher added

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,16 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'lowitea/aw-watcher.nvim',
+    opts = { -- required, but can be empty table: {}
+      -- add any options here
+      -- for example:
+      aw_server = {
+        host = '127.0.0.1',
+        port = 5600,
+      },
+    },
+  },
+}
