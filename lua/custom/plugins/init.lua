4f4f040f9361f48de679650157a20bdf17fcf7e4
Luís Santos <luissantos.lfsg@gmail.com>
2025-04-02 11:58:26 +0100

    seting up lsp's and clipboard

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,16 +2,4 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {
-  {
-    'lowitea/aw-watcher.nvim',
-    opts = { -- required, but can be empty table: {}
-      -- add any options here
-      -- for example:
-      aw_server = {
-        host = '127.0.0.1',
-        port = 5600,
-      },
-    },
-  },
-}
+return {}
